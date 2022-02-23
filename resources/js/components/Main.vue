5ce178808ea2f1d9b2b41d478c900b55cc2ee157
jimmyhu997 <jimmywx@outlook.it>
2022-02-23 18:08:20 +0100

    base li api call

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>Main</h2>
+
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                <h3>{{post.title}}</h3>
+                <p>{{post.content}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+        return {
+            posts: [],
+        }
+    },
+    created(){
+        axios.get('api/posts')
+            .then((response) => {
+                this.posts = response.data
+            })
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/admin.js', 'public/js')
+    .sass('resources/sass/admin.scss', 'public/css');
+mix.js('resources/js/front_office.js', 'public/js')
+    .sass('resources/sass/front_office.scss', 'public/css');
